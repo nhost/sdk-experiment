2d039bea6da22bac15a9bf94dffc14c47fd72cab
David Barroso <dbarrosop@dravetech.com>
2025-05-26 16:40:09 +0200

    asd

--- tools/codegen/cmd/gen/gen.go
+++ tools/codegen/cmd/gen/gen.go
@@ -3,14 +3,18 @@ package gen
 import (
 	"context"
 	"fmt"
+	"os"
 
+	"github.com/nhost/sdk-experiment/tools/codegen/processor"
+	"github.com/nhost/sdk-experiment/tools/codegen/processor/typescript"
+	"github.com/pb33f/libopenapi"
 	"github.com/urfave/cli/v3"
 )
 
 const (
 	flagOpenAPIFile = "openapi-file"
 	flagOutputFile  = "output-file"
-	flagGenerator   = "generator"
+	flagPlugin      = "plugin"
 )
 
 func Command() *cli.Command {
@@ -32,16 +36,59 @@ func Command() *cli.Command {
 				Sources:  cli.EnvVars("OUTPUT_FILE"),
 			},
 			&cli.StringFlag{ //nolint:exhaustruct
-				Name:     flagGenerator,
-				Usage:    "Generator to use",
+				Name:     flagPlugin,
+				Usage:    "Plugin to use. Supported: typescript",
 				Required: true,
-				Sources:  cli.EnvVars("GENERATOR"),
+				Sources:  cli.EnvVars("PLUGIN"),
 			},
 		},
 	}
 }
 
-func action(_ context.Context, _ *cli.Command) error {
-	fmt.Println("Starting code generation...") //nolint:forbidigo
+func action(_ context.Context, c *cli.Command) error {
+	fmt.Println("Generating code...") //nolint:forbidigo
+
+	var p processor.Plugin
+	switch c.String(flagPlugin) {
+	case "typescript":
+		p = &typescript.Typescript{}
+	default:
+		return cli.Exit("unsupported plugin: %s"+c.String(flagPlugin), 1)
+	}
+
+	b, err := os.ReadFile(c.String(flagOpenAPIFile))
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to read OpenAPI file: %v", err), 1)
+	}
+
+	document, err := libopenapi.NewDocument(b)
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to parse OpenAPI document: %v", err), 1)
+	}
+	docModel, errors := document.BuildV3Model()
+	if len(errors) > 0 {
+		for i := range errors {
+			fmt.Printf("error: %e\n", errors[i]) //nolint:forbidigo
+		}
+		return cli.Exit("failed to build OpenAPI model", 1)
+	}
+
+	ir, err := processor.NewInterMediateRepresentation(docModel, p)
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to create intermediate representation: %v", err), 1)
+	}
+
+	f, err := os.OpenFile(c.String(flagOutputFile), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644) //nolint:mnd
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("failed to open output file: %v", err), 1)
+	}
+	defer f.Close()
+
+	if err := ir.Render(f); err != nil {
+		return cli.Exit(fmt.Sprintf("failed to write output: %v", err), 1)
+	}
+
+	fmt.Printf("Code generated successfully to %s\n", c.String(flagOutputFile)) //nolint:forbidigo
+
 	return nil
 }
